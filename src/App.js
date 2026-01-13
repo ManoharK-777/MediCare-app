78f4b149fa458cb08ae30e554f17fbfa549679ac
K.Manohar <kmanohar17072007@gmail.com>
2026-01-13 12:41:52 +0530

    Initialize React app structure and add Netlify config

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>MediCare App</h1>
+        <p>Welcome to the MediCare application. Replace this with your app's UI.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
