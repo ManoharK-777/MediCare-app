5579269404f9e3c6e0e7a9ee733757277010abd3
K.Manohar <kmanohar17072007@gmail.com>
2026-01-13 12:29:10 +0530

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,35 @@
+const translations = {
+  en: {
+    login: "Login",
+    signup: "Sign Up",
+    caregiver: "I am a Caregiver",
+    senior: "I am a Senior",
+    enterCode: "Enter Caregiver Code",
+    connect: "Connect",
+    addMedicine: "Add Medicine",
+    medicineName: "Medicine Name",
+    dosage: "Dosage",
+    time: "Time",
+    yourMedicines: "Your Medicines",
+    takeNow: "Take Now",
+    taken: "I HAVE TAKEN IT",
+  },
+
+  te: {
+    login: "లాగిన్",
+    signup: "సైన్ అప్",
+    caregiver: "నేను సంరక్షకుడిని",
+    senior: "నేను పెద్దవాడిని",
+    enterCode: "కేర్ గివర్ కోడ్ ఇవ్వండి",
+    connect: "కనెక్ట్ చేయండి",
+    addMedicine: "మందు జోడించండి",
+    medicineName: "మందు పేరు",
+    dosage: "మోతాదు",
+    time: "సమయం",
+    yourMedicines: "మీ మందులు",
+    takeNow: "ఇప్పుడు తీసుకోండి",
+    taken: "నేను తీసుకున్నాను",
+  },
+};
+
+export default translations;
